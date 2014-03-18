4132151cc0188ae7fd63dbb935b6238f233cf2e2
Valeriy Sokoloff <v.v.sokoloff@gmail.com>
2014-03-18 18:55:51 +0200

    rails_apps_composer: testing framework

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,12 @@ Bundler.require(*Rails.groups)
 
 module RailsTeamviewerApiDummy
   class Application < Rails::Application
+
+    config.generators do |g|
+      
+      
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
