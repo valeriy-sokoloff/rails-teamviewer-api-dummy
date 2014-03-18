4f652b368846918874e2d76acdb36bbc1f4b1a1b
Valeriy Sokoloff <v.v.sokoloff@gmail.com>
2014-03-18 18:49:34 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTeamviewerApiDummy::Application.config.secret_key_base = '1a06d99773f80fde2cacd23e0fd83550ae9f7068b08e57a413b507f1b74b2809e93541a38fb103350fe17620ab48ea1334333224e48daaecaf01b897ab3760ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsTeamviewerApiDummy::Application.config.session_store :cookie_store, key: '_rails-teamviewer-api-dummy_session'
