e57d64aedc435de24e6a385a9600538bd299721a
Valeriy Sokoloff <v.v.sokoloff@gmail.com>
2014-03-19 17:17:21 +0200

    wrapped api methods

--- lib/teamviewer_client.rb
+++ lib/teamviewer_client.rb
@@ -11,6 +11,7 @@ class TeamviewerClient
     OAuth2::AccessToken.new(@client, str)
   end
 
+
   #------------#
   #--- Ping ---#
   #------------#
@@ -20,12 +21,116 @@ class TeamviewerClient
   end
 
 
-  #private
+  #-------------#
+  #--- Users ---#
+  #-------------#
 
-  def get(resource)
-    response = @access_token.get(resource)
-    response.parsed
+  def users(params = nil)
+    get 'api/v1/users', params
+  end
+
+  def user_add(data)
+    post 'api/v1/users', data
+  end
+
+  def user_get(id)
+    get "api/v1/users/#{id}"
+  end
+
+  def user_update(id, data)
+    put "api/v1/users/#{id}", data
   end
 
 
-end
+  #----------------#
+  #--- Sessions ---#
+  #----------------#
+
+  def sessions(params = nil)
+    get 'api/v1/sessions', params
+  end
+
+  def session_add(data)
+    post 'api/v1/sessions', data
+  end
+
+  def session_get(code)
+    get "api/v1/sessions/#{code}"
+  end
+
+  def session_update(code)
+    put "api/v1/sessions/#{code}", data
+  end
+
+
+  #--------------------------#
+  #--- Connection Reports ---#
+  #--------------------------#
+
+  def reports(params = nil)
+    get 'api/v1/reports/connections', params
+  end
+
+  def report_update(id, data)
+    put "api/v1/reports/connections/#{id}", data
+  end
+
+  def report_delete(id)
+    delete "api/v1/reports/connections/#{id}"
+  end
+
+
+  #----------------#
+  #--- Meetings ---#
+  #----------------#
+
+  def meetings(params = nil)
+    get 'api/v1/meetings', params
+  end
+
+  def meeting_get(id)
+    get "api/v1/meetings/#{id}"
+  end
+
+  def meeting_invitation(id, params)
+    get "api/v1/meetings/#{id}/invitation", params
+  end
+
+  def meeting_new(data)
+    post 'api/v1/meetings', data
+  end
+
+  def meeting_update(id, data)
+    put "api/v1/meetings/#{id}", data
+  end
+
+  def meeting_delete(id)
+    delete "api/v1/meetings/#{id}"
+  end
+
+
+
+  #private
+
+  def get(resource, params = nil)
+    response = @access_token.get(resource, :params => params)
+    response.parsed
+  end
+
+  def post(resource, data)
+    response = @access_token.post( resource, JSON.generate(data) )
+    response.status == 200 ? response.parsed : response.status
+  end
+
+  def put(resource, data)
+    response = @access_token.put( resource, JSON.generate(data) )
+    response.status
+  end
+
+  def delete(resource)
+    @access_token.delete( resource )
+  end
+
+
+
+  end
